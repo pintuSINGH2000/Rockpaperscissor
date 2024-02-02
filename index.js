3c61796a4cb880d67da323fc3311229168f8109d
pintuSINGH2000 <94526600+pintuSINGH2000@users.noreply.github.com>
2024-02-02 20:28:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,158 +1,148 @@
-
-var items = document.querySelectorAll('.item');
-const page = document.getElementById('page1');
-const result = document.getElementById('result');
-const userPick = document.querySelector(".user-picked");
-const pcPick = document.querySelector(".computer-picked");
-const resultagainst = document.querySelector(".result-against");
-const msg = document.querySelector('.message');
-const circle = document.getElementById('circle');
-const resultPage = document.querySelector('.result');
-const game = document.getElementById('game-container');
-const celebration = document.getElementById('celebration-container');
-const next = document.getElementById('next');
-const pcWinner = document.getElementById('pc-winner');
-const userWinner = document.getElementById('user-winner');
-const rock1 = document.getElementById('rock1');
-const rock2 = document.getElementById('rock2');
-const paper1 = document.getElementById('paper1');
-const paper2 = document.getElementById('paper2');
-const scissor1 = document.getElementById('scissor1');
-const scissor2 = document.getElementById('scissor2');
-const winner1 = document.querySelectorAll('.user-winner');
-const winner2 = document.querySelectorAll('.pc-winner');
-const rules = document.querySelector('.rules');
+var items = document.querySelectorAll(".item");
+const page = document.getElementById("page1");
+const result = document.getElementById("result");
+const resultagainst = document.querySelectorAll(".result-against");
+const msg = document.querySelectorAll(".message");
+const game = document.getElementById("game-container");
+const celebration = document.getElementById("celebration-container");
+const next = document.getElementById("next");
+const pcWinner = document.getElementById("pc-winner");
+const userWinner = document.getElementById("user-winner");
+const winner1 = document.querySelectorAll(".user-winner");
+const winner2 = document.querySelectorAll(".pc-winner");
+const userChoice = document.querySelector(".user-choice");
+const pcChoice = document.querySelector(".pc-choice");
+const footer = document.querySelector(".footer");
+const rules = document.querySelector(".rules");
 const prevResultObject = localStorage.getItem("rockpaperscissor");
-const TotalcomputerScore=document.getElementById('computer-score');
-const TotaluserScore=document.getElementById('user-score');
+const TotalcomputerScore = document.querySelector(".pc-current-score");
+const TotaluserScore = document.querySelector(".user-current-score");
 var userScore = 0;
 var computerScore = 0;
-const setScore = (user,computer) =>{
-    if(TotalcomputerScore!=null){
-        TotalcomputerScore.value=computer;
-    }
-
-    if(TotaluserScore!=null){
-        TotaluserScore.value=user;
-    }
-}
-if(prevResultObject){
-    const prevResult = JSON.parse(prevResultObject);
-    userScore = prevResult.userScore;
-    computerScore = prevResult.computerScore;
-    setScore(userScore,computerScore);
+const setScore = (user, computer) => {
+  TotalcomputerScore.textContent = computer;
+  TotaluserScore.textContent = user;
+};
+if (prevResultObject) {
+  const prevResult = JSON.parse(prevResultObject);
+  userScore = prevResult.userScore;
+  computerScore = prevResult.computerScore;
+  setScore(userScore, computerScore);
 }
 
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    const randomIndex = Math.floor(Math.random() * items.length);
+    const randomitem = items[randomIndex];
+    page.style.display = "none";
+    rules.style.visibility = "hidden";
+    var ans = 0;
+    if (item.id === "rock") {
+      if (randomitem.id == "paper") {
+        ans = -1;
+      } else if (randomitem.id == "scissor") {
+        ans = 1;
+      }
+    } else if (item.id === "paper") {
+      if (randomitem.id == "scissor") {
+        ans = -1;
+      } else if (randomitem.id == "rock") {
+        ans = 1;
+      }
+    } else {
+      if (randomitem.id == "rock") {
+        ans = -1;
+      } else if (randomitem.id == "paper") {
+        ans = 1;
+      }
+    }
 
+    if (ans === 1) {
+      userScore = userScore + 1;
+      msg.forEach((msg) => {
+        msg.textContent += "You Win";
+      });
+      resultagainst.forEach((resultagainst) => {
+        resultagainst.style.display = "block";
+      });
+      next.style.display = "inline";
+      winner1.forEach((winner) => {
+        winner.style.display = "block";
+      });
+    } else if (ans === -1) {
+      computerScore = computerScore + 1;
+      msg.forEach((msg) => {
+        msg.textContent += "You Lost";
+      });
+      resultagainst.forEach((resultagainst) => {
+        resultagainst.style.display = "block";
+      });
+      winner2.forEach((winner) => {
+        winner.style.display = "block";
+      });
+    } else {
+      msg.forEach((msg) => {
+        msg.textContent += "Tie Up";
+      });
+      resultagainst.forEach((resultagainst) => {
+        resultagainst.style.display = "none";
+      });
+    }
+    setScore(userScore, computerScore);
 
-items.forEach(item => {
-    item.addEventListener('click', function () {
-        const randomIndex = Math.floor(Math.random() * items.length);
-        const randomitem = items[randomIndex];
-        page.style.display = "none";
-        rules.style.visibility = 'hidden';
-        var ans=0;
-        if(item.id==='rock'){
-            rock1.style.display = 'grid';
-            if(randomitem.id=='paper'){
-                paper2.style.display = 'flex';
-                ans=-1;
-            }else if(randomitem.id=='scissor'){
-                scissor2.style.display = 'flex';
-                ans=1;
-            }else{
-                rock2.style.display = 'flex';
-            }
-        }else if(item.id==='paper'){
-            paper1.style.display = 'grid';
-            if(randomitem.id=='scissor'){
-                scissor2.style.display = 'flex';
-                ans=-1;
-            }else if(randomitem.id=='rock'){
-                rock2.style.display = 'flex';
-                ans=1;
-            }else{
-                paper2.style.display = 'flex'
-            }
-        }else{
-            scissor1.style.display = 'grid';
-            if(randomitem.id=='rock'){
-                rock2.style.display = 'flex';
-                ans=-1;
-            }else if(randomitem.id=='paper'){
-                paper2.style.display = 'flex';
-                ans=1;
-            }else{
-                scissor2.style.display = 'flex';
-            }
-        }
+    const duplicateRandomitem = randomitem.cloneNode(true);
+    const duplicateitem = item.cloneNode(true);
+    duplicateRandomitem.classList.add("item1");
+    duplicateitem.classList.add("item1");
+    duplicateRandomitem.querySelector(".circle").classList.add("circle2");
+    duplicateitem.querySelector(".circle").classList.add("circle2");
+    userChoice.appendChild(duplicateitem);
+    pcChoice.appendChild(duplicateRandomitem);
+    result.style.display = "flex";
 
-        if(ans===1){
-            userScore = userScore + 1;
-            msg.textContent  += "You Win";
-            resultagainst.style.display = "block";
-            next.style.display= 'inline';
-            winner1.forEach(winner => {
-                winner.style.display = 'block';
-            });
-          
-        }else if(ans===-1){
-            computerScore = computerScore + 1;
-            msg.textContent  += "You Lost";
-            resultagainst.style.display = "block";
-            winner2.forEach(winner => {
-                winner.style.display = 'block';
-            });
-        }else{
-            msg.textContent  += "Tie Up";
-            resultagainst.style.display = "none";
-        }
-        setScore(userScore,computerScore);
-        
-        userPick.style.display = "block";
-        pcPick.style.display = "block";
-        result.style.display = "block";
-        resultPage.style.display = "flex";
-        localStorage.setItem("rockpaperscissor",JSON.stringify({"userScore":userScore,"computerScore":computerScore}));
-    });
+    localStorage.setItem(
+      "rockpaperscissor",
+      JSON.stringify({ userScore: userScore, computerScore: computerScore })
+    );
+  });
 });
 
-const playAgain = () =>{
-    paper1.style.display = 'none';
-    paper2.style.display = 'none';
-    scissor1.style.display = 'none';
-    scissor2.style.display = 'none';
-    rock1.style.display = 'none';
-    rock2.style.display = 'none';
-    game.style.display = 'block';
-    celebration.style.display = 'none';
-    resultagainst.style.display = 'none';
-    userPick.style.display = 'none';
-    pcPick.style.display = 'none';
-    result.style.display = 'none';
-    msg.textContent = '';
-    page.style.display = 'block';
-    next.style.display= 'none';
-    winner1.forEach(winner => {
-        winner.style.display = 'none';
-    });
-    winner2.forEach(winner => {
-        winner.style.display = 'none';
-    });
-}
+const playAgain = () => {
+  userChoice.removeChild(userChoice.lastChild);
+  pcChoice.removeChild(pcChoice.lastChild);
+  page.style.display = "block";
+  result.style.display = "none";
+  game.style.display = "flex";
+  celebration.style.display = "none";
+  winner1.forEach((winner) => {
+    winner.style.display = "none";
+  });
+  winner2.forEach((winner) => {
+    winner.style.display = "none";
+  });
+  msg.forEach((msg) => {
+    msg.textContent = "";
+  });
 
+  next.style.display = "none";
+};
 
 const ruleClose = () => {
-   rules.style.visibility = 'hidden';
-}
+  rules.style.zIndex = "0";
+  page.style.zIndex = "1";
+  result.style.zIndex = "1";
+  rules.style.visibility = "hidden";
+};
 
 const ruleOpen = () => {
-    rules.style.visibility = 'visible';
-}
+  page.style.zIndex = "0";
+  result.style.zIndex = "0";
+  rules.style.zIndex = "1";
+  rules.style.visibility = "visible";
+};
 
 const openCelebration = (element) => {
-    element.style.display = "none";
-    game.style.display = 'none';
-    celebration.style.display = 'flex';
-}
-
+  element.style.display = "none";
+  game.style.display = "none";
+  celebration.style.display = "flex";
+};
